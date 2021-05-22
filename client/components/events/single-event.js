c0f9b6be29a4cd143403f7de6a8f3a028d98c20f
Grant Weiss <grant.sweiss@gmail.com>
2021-05-22 19:15:06 -0400

    add query for users containing skills for an event (#13)

--- client/components/events/single-event.js
+++ client/components/events/single-event.js
@@ -3,36 +3,80 @@ import {connect} from 'react-redux'
 import {withRouter} from 'react-router'
 import Container from 'react-bootstrap/Container'
 import {fetchSingleEvent} from '../../store/single-event'
+import {fetchUsers} from '../../store/users'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class SingleEvent extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
+    this.getServices = this.getServices.bind(this)
+    this.getUserServices = this.getUserServices.bind(this)
   }
-  componentDidMount() {
+  async componentDidMount() {
     const id = parseInt(this.props.match.params.id, 10)
-    this.props.fetchSingleEvent(id)
+    await this.props.fetchSingleEvent(id)
+    this.props.fetchUsers()
+  }
+  getServices() {
+    const event = this.props.singleEvent
+    if (event) {
+      return event.services
+    }
+    return []
+  }
+  getUserServices(userSkills) {
+    const services = this.getServices()
+    const serviceNames = services.reduce((accum, curr) => {
+      accum.push(curr.title)
+      return accum
+    }, [])
+
+    return userSkills.some(({title}) => serviceNames.includes(title))
   }
   render() {
-    const {singleEvent, eventType} = this.props
+    const {singleEvent, eventType, users} = this.props
     return (
       <Container>
+        <Card style={{width: '18rem'}} className="center">
+          <Card.Body>
+            <Card.Title>
+              {singleEvent && singleEvent.eventType && eventType.name}
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {singleEvent.location}
+            </Card.Subtitle>
+            Pending Services:
+            {singleEvent &&
+            singleEvent.services &&
+            singleEvent.services.length ? (
+              singleEvent.services.map(service => (
+                <div key={service.id}>{service.title}</div>
+              ))
+            ) : (
+              <div>No Services yet</div>
+            )}
+          </Card.Body>
+        </Card>
         <div>
-          <p>
-            Event type: {singleEvent && singleEvent.eventType && eventType.name}
-          </p>
-          <p>Location: {singleEvent.location}</p>
-        </div>
-        <div>
-          <p>Pending Services:</p>
-          {singleEvent &&
-          singleEvent.services &&
-          singleEvent.services.length ? (
-            singleEvent.services.map(service => (
-              <p key={service.id}>{service.title}</p>
-            ))
+          <br />
+          <h3>Search for Service Providers</h3>
+          {users && users.length ? (
+            users.map(user => {
+              if (this.getUserServices(user.skills)) {
+                return (
+                  <Card key={user.id} style={{marginBottom: 10}}>
+                    <Card.Body>
+                      <Card.Title>{user.email}</Card.Title>
+                      <Button variant="primary">Request a Quote!</Button>
+                    </Card.Body>
+                  </Card>
+                )
+              }
+            })
           ) : (
-            <p>No Services yet</p>
+            <p>No users</p>
           )}
         </div>
       </Container>
@@ -42,11 +86,13 @@ class SingleEvent extends React.Component {
 
 const mapState = state => ({
   singleEvent: state.singleEvent,
-  eventType: state.singleEvent.eventType
+  eventType: state.singleEvent.eventType,
+  users: state.users
 })
 
 const mapDispatch = dispatch => ({
-  fetchSingleEvent: id => dispatch(fetchSingleEvent(id))
+  fetchSingleEvent: id => dispatch(fetchSingleEvent(id)),
+  fetchUsers: () => dispatch(fetchUsers())
 })
 
 export default withRouter(connect(mapState, mapDispatch)(SingleEvent))
