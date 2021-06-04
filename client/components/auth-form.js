36c4fa02d656be115e31fe0323d2f9c38649e109
Grant Weiss <grant.sweiss@gmail.com>
2021-06-04 13:53:17 -0500

    fix login css (#26)

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -9,12 +9,16 @@ const AuthForm = () => {
         <h1 className="auth-text">Huddle</h1>
       </div>
       <div className="auth-info">
-        <h2 className="auth-info-text">Enabling providers</h2>
-        <h5 className="auth-info-text">For the best events</h5>
+        <h4 className="auth-info-text">Enabling providers</h4>
+        <h2 className="auth-info-text">
+          <strong>For the best events</strong>
+        </h2>
+        <br />
+        <h5 className="auth-info-text">Plan {'&'} enjoy in half the time</h5>
         <br />
       </div>
       <Button className="auth0Button" href="/auth/auth0">
-        Login
+        Log in
       </Button>
     </div>
   )
