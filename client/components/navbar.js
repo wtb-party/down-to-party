715415a7ee7ea7fc213989bb824a311cc8508bd7
Grant Weiss <grant.sweiss@gmail.com>
2021-07-14 00:12:07 -0500

    update nav styles

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import {NavLink} from 'react-router-dom'
-import {logout} from '../store'
-import Navbar from 'react-bootstrap/Navbar'
+import Image from 'react-bootstrap/Image'
 import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import {useDispatch, useSelector} from 'react-redux'
+import {Link, NavLink} from 'react-router-dom'
+import {logout} from '../store'
 
 const mainNav = () => {
   const dispatch = useDispatch()
   const handleClick = () => dispatch(logout())
   const isLoggedIn = useSelector(state => !!state.user.user.id)
-  const userId = useSelector(state => state.user.user.id)
+  const user = useSelector(state => state.user.user)
 
   return (
-    <Navbar bg="light" expand="lg" id="nav">
+    <Navbar bg="light" expand="lg" id="nav" collapseOnSelect={true}>
       <Navbar.Brand as={NavLink} to="/home">
         Huddle
       </Navbar.Brand>
@@ -21,21 +23,55 @@ const mainNav = () => {
         <Nav className="ml-auto">
           {isLoggedIn ? (
             <>
-              <Nav.Link as={NavLink} to="/home">
+              <Nav.Link
+                as={NavLink}
+                className="link"
+                eventKey="home"
+                to="/home"
+              >
                 Home
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/providers">
+              <Nav.Link
+                as={NavLink}
+                className="link"
+                eventKey="providers"
+                to="/providers"
+              >
                 Providers
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/listings">
+              <Nav.Link
+                as={NavLink}
+                className="link"
+                eventKey="listings"
+                to="/listings"
+              >
                 Listings
               </Nav.Link>
-              <Nav.Link as={NavLink} to={`/users/${userId}/profile`}>
-                Profile
-              </Nav.Link>
-              <Nav.Link href="/auth/auth0/logout" onClick={handleClick}>
-                Logout
-              </Nav.Link>
+              <NavDropdown
+                title={
+                  <Image
+                    src={user.photoURL}
+                    className="nav-image"
+                    roundedCircle
+                  />
+                }
+                id="nav-dropdown"
+                alignRight
+              >
+                <NavDropdown.Item
+                  as={Link}
+                  eventKey="profile"
+                  to={`/users/${user.id}/profile`}
+                >
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  onClick={handleClick}
+                  href="/auth/auth0/logout"
+                >
+                  Sign out
+                </NavDropdown.Item>
+              </NavDropdown>
             </>
           ) : (
             <>
