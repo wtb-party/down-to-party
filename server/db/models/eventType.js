6d780e28f3a31872e4dea4366f28dad5140cf154
Grant Weiss <grant.sweiss@gmail.com>
2021-07-13 20:34:48 -0500

    update all models to use UUID4 - strip components of parseInt

--- server/db/models/eventType.js
+++ server/db/models/eventType.js
@@ -2,6 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const EventType = db.define('eventType', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  },
   name: {
     type: Sequelize.STRING
   }
